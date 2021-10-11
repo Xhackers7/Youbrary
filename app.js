dca2329274619309934eee8438343b216d17df65
Xhackers7 <nishaajithkj@gmail.com>
2021-10-11 20:40:03 +0530

    Added authors route and a database model for authors

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-if (process.env.NODE_ENV !== 'production') require('dotenv').config()
-
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 
 const app = express()
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(express.urlencoded({limit: "10mb", extended: false}))
 
 mongoose.connect(process.env.DATABASE_URL)
 
@@ -21,5 +24,6 @@ db.on('error', err => console.error(err))
 db.once('open', () => console.log('Connected to database!'))
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
